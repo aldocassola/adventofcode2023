e512c855b98028639f0b3a8b96e121ade0bded7f
Aldo Cassola <aldocassola@gmail.com>
2023-12-10 09:26:57 -0500

    solve day2 part2

--- day2/src/bin/run2.rs
+++ day2/src/bin/run2.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::env;
+use std::fs;
+use day2::sum_games_power;
+
+fn main() -> Result<(), String> {
+    let args = env::args().collect::<Vec<_>>();
+
+    if args.len() != 2 {
+        return Err("invalid arguments".to_owned());
+    }
+
+    let filename = args[1].to_owned();
+    let input = fs::read_to_string(filename).unwrap();
+    println!("{}", sum_games_power(&input));
+    Ok(())
+}
--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -23,25 +23,23 @@ impl FromStr for Color {
     }
 }
 
+fn parse_ball_count(count_color: &str) -> (i32, Color) {
+    let num_color = count_color
+        .trim_start()
+        .trim_end()
+        .split(' ')
+        .collect::<Vec<_>>();
+
+    (
+        num_color[0].parse::<i32>().unwrap(),
+        num_color[1].parse::<Color>().unwrap(),
+    )
+}
+
 fn is_possible(game_str: &str, config: &Vec<i32>) -> bool {
     for trial in game_str.split(';') {
         for count_ball in trial.split(',') {
-            let color_ball = count_ball.trim_start().trim_end().split(' ').collect::<Vec<_>>();
-
-            if color_ball.len() != 2 {
-                eprint!("bad ball count:{:?}", color_ball);
-                return false;
-            }
-
-            let count = match color_ball[0].parse::<i32>() {
-                Ok(c) => c,
-                Err(msg) => {
-                    eprint!("bad count:{msg}");
-                    return false;
-                }
-            };
-
-            let color = color_ball[1].parse::<Color>().expect("bad color");
+            let (count, color) = parse_ball_count(count_ball);
 
             if config[color as usize] < count {
                 return false;
@@ -52,15 +50,58 @@ fn is_possible(game_str: &str, config: &Vec<i32>) -> bool {
     true
 }
 
+pub fn sum_games_power(games: &str) -> i64 {
+    games.lines()
+        .map(|l| game_power(l))
+        .sum()
+}
+
+fn game_power(game: &str) -> i64 {
+    game_min_config(game).iter()
+        .fold(1 as i64, |acc, val | acc * val.to_owned() as i64 )
+
+}
+
+fn game_min_config(game_str: &str) -> Vec<i32> {
+    game_body_min_config(game_str.split(':').last().unwrap())
+}
+
+fn game_body_min_config(game_body_str: &str) -> Vec<i32> {
+    let mut red: i32 = 0;
+    let mut green: i32 = 0;
+    let mut blue: i32 = 0;
+
+    for trial in game_body_str.split(';') {
+        for ball_count in trial.split(',') {
+            let (count, color) = parse_ball_count(ball_count);
+            match color {
+                Color::Red => {
+                    if count > red {
+                        red = count;
+                    }
+                }
+                Color::Green => {
+                    if count > green {
+                        green = count;
+                    }
+                }
+                Color::Blue => {
+                    if count > blue {
+                        blue = count;
+                    }
+                }
+            }
+        }
+    }
+
+    vec![red, green, blue]
+}
+
 pub fn possible_game_sum(input: &str, config: &Vec<i32>) -> i32 {
     let mut possible = Vec::<i32>::new();
 
     for game in input.lines() {
         let head_body = game.split(":").collect::<Vec<_>>();
-        if head_body.len() != 2 {
-            eprintln!("bad game {game}");
-            continue;
-        }
 
         let game_number = head_body[0].split(" ").collect::<Vec<_>>();
         let game_id = game_number[1].parse::<i32>().expect("should be a number");
@@ -91,5 +132,31 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
         config[Color::Green as usize] = 13;
         config[Color::Blue as usize] = 14;
         assert_eq!(possible_game_sum(&input, &config), 8);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let expected = vec![
+            vec![4, 2, 6],
+            vec![1, 3, 4],
+            vec![20, 13, 6],
+            vec![14, 3, 15],
+            vec![6, 3, 2],
+        ];
+        let expected_power = vec![
+            48, 12, 1560, 630, 36
+        ];
+
+        for (pos, ln) in input.lines().enumerate() {
+            assert_eq!(expected[pos], game_min_config(ln));
+            assert_eq!(expected_power[pos], game_power(ln));
+        }
+
+        assert_eq!(2286, sum_games_power(input));
     }
 }
